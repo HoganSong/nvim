a0d3026b0ac08478736de441e1b807e96e591c17
HoganSong <hogan0814@snu.ac.kr>
2026-04-09 13:36:40 +0900

    osc52 clipboard added

--- init.lua
+++ init.lua
@@ -19,3 +19,15 @@ require("nvim-tree").setup({
     dotfiles = true,
   },
 })
+
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
